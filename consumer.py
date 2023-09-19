d1f934552d7c2fbae7de85b946f6fbc4edee938a
Ossi Rajuvaara <ossi@robocorp.com>
2023-09-19 11:58:45 +0300

    Run black

--- consumer.py
+++ consumer.py
@@ -3,27 +3,37 @@ from robocorp import workitems
 
 import requests
 
+
 @task
 def consume_traffic_data():
     """
-        Inhuman Insurance, Inc. Artificial Intelligence System robot.
-        Consumes traffic data work items.
+    Inhuman Insurance, Inc. Artificial Intelligence System robot.
+    Consumes traffic data work items.
     """
     process_traffic_data()
+
 
 def process_traffic_data():
     for item in workitems.inputs:
         payload = item.payload
         traffic_data = payload["traffic_data"]
         valid = validate_traffic_data(traffic_data)
-        if(valid):
+        if valid:
             status, return_json = post_traffc_data_to_sales_system(traffic_data)
             if status == 200:
                 item.done
-            else: 
-                item.fail(exception_type="APPLICATION", code="TRAFFIC_DATA_POST_FAILED", message=return_json["message"])
+            else:
+                item.fail(
+                    exception_type="APPLICATION",
+                    code="TRAFFIC_DATA_POST_FAILED",
+                    message=return_json["message"],
+                )
         else:
-            item.fail(exception_type="BUSINESS", code="INVALID_TRAFFIC_DATA", message=item.payload)
+            item.fail(
+                exception_type="BUSINESS",
+                code="INVALID_TRAFFIC_DATA",
+                message=item.payload,
+            )
 
 
 def validate_traffic_data(traffic_data):
@@ -33,7 +43,7 @@ def validate_traffic_data(traffic_data):
     else:
         return False
 
-    
+
 def post_traffc_data_to_sales_system(traffic_data):
     url = "https://robocorp.com/inhuman-insurance-inc/sales-system-api"
     response = requests.post(url, json=traffic_data)
--- producer.py
+++ producer.py
@@ -16,26 +16,34 @@ YEAR_KEY = "TimeDim"
 RATE_KEY = "NumericValue"
 GENDER_KEY = "Dim1"
 
+
 @task
 def produce_traffic_data():
     """
-        Inhuman Insurance, Inc. Artificial Intelligence System robot.
-        Produces traffic data work items.
+    Inhuman Insurance, Inc. Artificial Intelligence System robot.
+    Produces traffic data work items.
     """
-    #download_traffic_data()
+    # download_traffic_data()
     traffic_data = load_traffic_data_as_table()
     filtered_data = filter_and_sort_traffic_data(traffic_data)
     filtered_data = get_latest_data_by_country(filtered_data)
     payloads = create_work_item_payloads(filtered_data)
     save_work_item_payloads(payloads)
-    
+
+
 def download_traffic_data():
-    http.download(url="https://github.com/robocorp/inhuman-insurance-inc/raw/main/RS_198.json", target_file=TRAFFIC_JSON_FILE_PATH, overwrite=True)
+    http.download(
+        url="https://github.com/robocorp/inhuman-insurance-inc/raw/main/RS_198.json",
+        target_file=TRAFFIC_JSON_FILE_PATH,
+        overwrite=True,
+    )
+
 
 def load_traffic_data_as_table():
     json_data = json.load_json_from_file(TRAFFIC_JSON_FILE_PATH)
     table_from_json = table.create_table(json_data["value"])
     return table_from_json
+
 
 def filter_and_sort_traffic_data(data):
     max_rate = 5.0
@@ -45,6 +53,7 @@ def filter_and_sort_traffic_data(data):
     table.sort_table_by_column(data, YEAR_KEY, False)
     return data
 
+
 def get_latest_data_by_country(data):
     data = table.group_table_by_column(data, COUNTRY_KEY)
     latest_data_by_country = []
@@ -53,18 +62,20 @@ def get_latest_data_by_country(data):
         latest_data_by_country.append(first_row)
     return latest_data_by_country
 
+
 def create_work_item_payloads(traffic_data):
     payloads = []
     for row in traffic_data:
         payload = dict(
-            country = row[COUNTRY_KEY],
-            year = row[YEAR_KEY],
-            rate = row[RATE_KEY]
-            )
+            country=row[COUNTRY_KEY],
+            year=row[YEAR_KEY],
+            rate=row[RATE_KEY],
+        )
         payloads.append(payload)
     return payloads
 
+
 def save_work_item_payloads(payloads):
     for payload in payloads:
-        variables = dict(traffic_data = payload)
+        variables = dict(traffic_data=payload)
         workitems.outputs.create(variables)
